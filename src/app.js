218181fab9c3ab84de88b8f2fb0827ee14f29e72
Nazila001 <103484801+Nazila001@users.noreply.github.com>
2022-04-30 23:54:21 +0430

    new changes

--- src/app.js
+++ src/app.js
@@ -31,6 +31,7 @@ function displayTemprature(response) {
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000)
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.date.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
     function searchDefault(city) {
