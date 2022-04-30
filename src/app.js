a4c414791958202defc815ae12051841828471ad
Nazila001 <103484801+Nazila001@users.noreply.github.com>
2022-04-30 15:49:10 +0430

    added search engine

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ function formatDate(timestamo){
 }
 
 function displayTemprature(response) {
+    console.log(response.data);
 
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
@@ -21,16 +22,30 @@ function displayTemprature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind")
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000)
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.date.weather[0].icon}@2x.png`);
 }
 
-let apiKey = "5569faf92417631e8d5bf083852d65c1";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+    function searchDefault(city) {
+        let apiKey = "5569faf92417631e8d5bf083852d65c1";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(displayTemprature);
+    }
 
+    function subCity(event) {
+        event.preventDefault();
+        let cityInputElement = document.querySelector("#city-input");
+        searchDefault(cityInputElement.value);
+    }
+    
+    searchDefault("Yazd");
 
-axios.get(apiUrl).then(displayTemprature);
+    let form = document.querySelector("#search-form");
+    form.addEventListener("submit", subCity);
