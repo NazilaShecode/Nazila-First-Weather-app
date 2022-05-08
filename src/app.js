c1ec9caf1ad2afe434e12c6c63b6420c180077d9
Nazila001 <103484801+Nazila001@users.noreply.github.com>
2022-05-08 22:02:03 +0430

    fixed some issues

--- src/app.js
+++ src/app.js
@@ -14,6 +14,47 @@ function formatDate(timestamo){
 }
 
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun","Mon","Tue","Tue","Wed","Thu","Fri","Sat"];
+    
+    return days[day];
+}
+
+
+function displayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    
+    forecast.forEach(function(forecastDay, index) {
+        if (index < 6) {
+            forecastHTML += `
+            <div class="col-2">
+                <div class="weather-forcast-date">${formatDay(forecastDay.dt)}</div>
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="40"/>
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max">
+                        ${Math.round(forecastDay.temp.max)}°
+                    </span>
+                    <span class="wearher-forecast-temperature-min">
+                        ${Math.round(forecastDay.temp.min)}° 
+                    </span>
+                </div>
+            </div>`;
+        }
+    });
+        
+    forecastHTML = forecastHTML + `<div>`; 
+    forecastElement.innerHTML = forecastHTML;
+}
+    
+function getForecast(coordinates) {
+    let apiKey = "5569faf92417631e8d5bf083852d65c1";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemprature(response) {
 
@@ -83,3 +124,4 @@ function displayTemprature(response) {
     celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
     searchDefault("Yazd");
+    displayForcast();
